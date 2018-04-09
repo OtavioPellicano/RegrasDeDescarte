f30ba5da1b0e90aaff3bf700722c80414beb3824
Otavio Pellicano <otavio.pellicano@gmail.com>
2018-04-09 09:35:23 -0300

    Terminei a validação dos campos esperados para TGR

--- ValidacaoCamposEsperadosTGR.cpp
+++ ValidacaoCamposEsperadosTGR.cpp
@@ -10,16 +10,47 @@ ValidacaoCamposEsperadosTGR::ValidacaoCamposEsperadosTGR(const QString &medicao,
 
 bool ValidacaoCamposEsperadosTGR::cabecalhoValido(const QString &medicao)
 {
-    if(medicao.split(mSep).size() != 80)
+
+    mStrMed = medicao;
+    mStrMed = mStrMed.remove("\"");
+    mStrMed = mStrMed.remove("\r");
+    mStrMed = mStrMed.remove("\n");
+    if(mStrMed.split(mSep).size() == 80)
+    {
+        if(mStrMed == "data;versao;enderecoipservidor;portaservidor;enderecoipcliente;maccliente;numbytesup;numbytesdown;numpacotes;tamanhopacotertt;tamanhopacotevazaodownudp;tamanhopacotevazaoupudp;numrequisicoesparalelas;intervalodisponibilidade;intervalomedicoes;latenciamedia;latenciamedia_crosstrafficup;latenciamedia_crosstrafficdown;latenciamedia_tinicio;latenciamedia_tfim;jittermedia;jittermedia_crosstrafficup;jittermedia_crosstrafficdown;jittermedia_tinicio;jittermedia_tfim;vazaodownmediatcp;vazaodownmediatcp_crosstrafficup;vazaodownmediatcp_crosstrafficdown;vazaodownmediatcp_tinicio;vazaodownmediatcp_tfim;vazaodownmediaudp;vazaodownmediaudp_crosstrafficup;vazaodownmediaudp_crosstrafficdown;vazaodownmediaudp_tinicio;vazaodownmediaudp_tfim;vazaoupmediatcp;vazaoupmediatcp_crosstrafficup;vazaoupmediatcp_crosstrafficdown;vazaoupmediatcp_tinicio;vazaoupmediatcp_tfim;vazaoupmediaudp;vazaoupmediaudp_crosstrafficup;vazaoupmediaudp_crosstrafficdown;vazaoupmediaudp_tinicio;vazaoupmediaudp_tfim;perdatotalrtt;perdatotalrtt_crosstrafficup;perdatotalrtt_crosstrafficdown;perdatotalrtt_tinicio;perdatotalrtt_tfim;perdatotalvazaodownudp;perdatotalvazaodownudp_crosstrafficup;perdatotalvazaodownudp_crosstrafficdown;perdatotalvazaodownudp_tinicio;perdatotalvazaodownudp_tfim;amostraslatencia;amostraslatencia_crosstrafficup;amostraslatencia_crosstrafficdown;amostrasjitter;amostrasjitter_crosstrafficup;amostrasjitter_crosstrafficdown;dns_crosstrafficup;dns_crosstrafficdown;temporesposta_www-google-com;temporesposta_www-outlook-com;temporesposta_www-gmail-com;temporesposta_www-facebook-com;temporesposta_www-uol-com-br;temporesposta_www-globo-com;temporesposta_www-netflix-com;temporesposta_www-youtube-com;disponibilidade;tipodeacesso;filename;traceroute_crosstrafficup;traceroute_crosstrafficdown;traceroute_returned_hostnames;traceroute_returned_ips;traceroute_response_time;traceroute_packets_per_hop")
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    else
+    {
         return false;
-    return true;
+    }
 }
 
 bool ValidacaoCamposEsperadosTGR::parametroValido(const QString &medicao)
 {
 
+    mStrMed = medicao;
+    mStrMed = mStrMed.remove("\"");
 
-    return true;
+    bool ok[4];
+
+    mAvailSuccesses = mStrMed.split(mSep)[71].toInt(ok);
+    mAvgRTT = mStrMed.split(mSep)[15].toFloat(ok + 1);
+    mJitter = mStrMed.split(mSep)[20].toFloat(ok + 2);
+    mPacketLoss = mStrMed.split(mSep)[45].toFloat(ok + 3);
+
+    if(ok[0] && ok[1] && ok[2] && ok[3])
+    {
+        return true;
+    }
+
+    return false;
 }
 
 }
--- ValidacaoCamposEsperadosTGR.h
+++ ValidacaoCamposEsperadosTGR.h
@@ -17,20 +17,20 @@ enum testeTGR {SCM_4_5, SCM_6_7, SCM_8, SCM_9};
  * preenchidos, se o tipo de campo é o esperado, se o valor informado está dentro
  * da variação esperada (min/max) e se o valor informado é valido.
  *
- * availSuccesses   (int)
- * avgRTT           (double)
- * dateTime         (YYYY-MM-DD HH:mm:ss)
- * deviceID         (str)
- * failures         (int)
- * jitter           (double)
- * macAddress       (str)
- * manufacturer     ()      //Tabela de Assinantes
- * packetLoss       (int)
- * packetSent       ()
- * softwareVersion  (int)
- * sourceIPAddress  (str)
- * successes        (int)
- * testPoint        (str)
+    * availSuccesses   (int)
+    * avgRTT           (float)
+    * dateTime         (YYYY-MM-DD HH:mm:ss)
+    * deviceID         (str)
+    * failures         (int)
+    * jitter           (float)
+    * macAddress       (str)
+    * manufacturer     (str)      //Tabela de Assinantes
+    * packetLoss       (float)
+    * packetSent       (int)
+    * softwareVersion  (str)
+    * sourceIPAddress  (str)
+    * successes        (int)
+    * testPoint        (str)
  *
  * TGR: Para os RawData da TGR disponibilizados pela solução da TGR
  * além da validação padrão o processo também avalia se contém “#” nos
@@ -49,16 +49,15 @@ private:
 
 private:
     int mAvailSuccesses;
-    double mAvgRTT;
+    float mAvgRTT;
     QString mStrDateTime;   //("YYYY-MM-DD HH:mm:ss")
-    int mFailures;
-    double mJitter;
-    int mPacketLoss;
-    int mSoftwareVersion;
-    int mSuccesses;
+    float mJitter;
+    float mPacketLoss;
+
 
     QString mSep;
     testeTGR mTipoTesteTGR;
+    QString mStrMed;
 
 
 
